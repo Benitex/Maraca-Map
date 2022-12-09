2cf8ccc72cfd77e6ffb9e776f1d1ceb3fdd957a1
Benitex <benitoapepe@yahoo.com.br>
2022-12-09 11:46:32 -0300

    Adicionada opção de deletar listas

--- lib/screens/point_of_interest_lists.dart
+++ lib/screens/point_of_interest_lists.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maraca_map/providers/point_of_interest_lists_provider.dart';
+import 'package:maraca_map/services/local_storage.dart';
 import 'package:maraca_map/screens/custom_point_of_interest_list.dart';
 import 'package:maraca_map/widgets/point_of_interest_lists/add_list_dialog.dart';
 
@@ -10,6 +11,8 @@ class PointOfInterestLists extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final lists = ref.watch(pointOfInterestListProvider);
+    final listsController = ref.read(pointOfInterestListProvider.notifier);
+    final localStorage = ref.read(localStorageProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -20,14 +23,29 @@ class PointOfInterestLists extends ConsumerWidget {
       body: ListView(
         children: [
           for (String listName in lists.keys)
-            GestureDetector(
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => CustomPointOfInterestList(
-                  listName: listName,
-                  idsList: lists[listName]!,
-                )),
+            Dismissible(
+              key: Key(listName),
+              onDismissed: (direction) {
+                listsController.removeList(listName);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("$listName foi removido.")),
+                );
+                localStorage.saveListNames([
+                  for (String list in lists.keys)
+                    if (listName != list) list,
+                ]);
+              },
+              background: Container(color: Colors.red),
+
+              child: GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => CustomPointOfInterestList(
+                    listName: listName,
+                    idsList: lists[listName]!,
+                  )),
+                ),
+                child: Card(child: ListTile(title: Text(listName))),
               ),
-              child: Card(child: ListTile(title: Text(listName))),
             ),
         ],
       ),
